725d803cb02fdf448175487c77e4d62ec10e745a
rmhorne <ryan@argos-2.local>
2016-12-02 16:38:55 -0500

    Added BAM framework to personal site

---
+++ bam/modules/standingrock/BAM-panels.php
@@ -0,0 +1,16 @@
+<div id='infoBox' class='nonMapOverlay'>
+    <b> Application Information </b>
+    <div id="infoBoxCloseBox" class="popupCloseCarte">x</div>
+    <hr />
+    <div id='infoBoxContent' class="overlaycontent">
+        </div>
+</div>
+
+
+<div id='dataBox' class='nonMapOverlay'>
+    <b> Database </b>
+    <hr />
+    <div id="dataBoxCloseBox" class="popupCloseCarte">x</div>
+    <br />
+    <div id='dataBoxContent' class="overlaycontent"></div>
+</div>
