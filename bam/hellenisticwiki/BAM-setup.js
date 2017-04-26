1c0f7f19259845f297e90347b943fb592b9ebea1
rmhorne <ryan@argos-2.local>
2017-04-25 22:52:26 -0400

    reimport

---
+++ bam/hellenisticwiki/BAM-setup.js
@@ -0,0 +1,49 @@
+ var bamConfigJson = 
+        {
+	"bamPageTitle": "Hellenistic Wiki",
+	"bamFavIocn": "images/favicon.ico",
+	"bamLeftPanelTitleHtml": "\"Hellenistic\" Royal Relationships In <a href=\"https://www.wikipedia.org/\" target=\"_blank\"> Wikipedia </a>",
+	"bamNavBarImageHtml": "<a href=\"http://dsps.lib.uiowa.edu/bam/\" target=\"_blank\"> <img src=\"images/BAM-icon.png\" title=\"BAM Icon\" alt=\"BAM Icon\"></a>",
+	"bamApplicationLicenseHtml": "<a rel=\"license\" href=\"http://creativecommons.org/licenses/by/4.0/\" target=\"_blank\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by/4.0/88x31.png\"/></a>",
+	"bamdataSizeAttribute": "Betweenness Centrality",
+	"bamSizeDivider": "3",
+	"bamNodeMinSize": "4",
+	"bamNodeMaxSize": "20",
+	"bamMapSetViewX": "41.09591",
+	"bamMapSetViewY": "10.7666",
+	"bamMapSetViewZoom": "5",
+	"bamMainDataLocation": "data/graph.json",
+	"bamSourceDataType": "gephi",
+	"bamToolTipTitle": "name",
+	"bamLinkElement": "links",
+	"bamStrokeWidth": "2",
+	"bamLatAttribute": "repLat",
+	"bamLonAttribute": "repLon",
+	"bamToolTipAttributes":{
+		"count": "Incoming Connections:",
+		"repLat": "Representative Latitude"
+	},
+	"bamNavButtons":{
+		"appinfo": "<button id=\"infoButton\" class=\"accordion\"><img src=\"images/info.svg\" class=\"accordianImage\" alt=\"info\">&nbsp;&nbsp;&nbsp;&nbsp;Application Information</button>",
+		"databaseButton": "<button id=\"mapDatabaseButton\" class=\"accordion\"><img src=\"images/list.svg\" class=\"accordianImage\" alt=\"list\">&nbsp;&nbsp;&nbsp;&nbsp;Full Database</button>"
+	},
+	"bamD3Attributes":{
+		"Modularity Class":"Modularity Class",
+		"In-Degree": "Incoming Connections",
+		"Degree": "Total Connections"
+	},
+	"bamD3AttributeTitle": "name",
+	"bamD3AttributeImage": "image",
+	"bamD3AttributeImageDefault": "images/Wikipedia-logo-v2.svg",
+	"bamD3AttributeTitleLink": "wiki_link",
+	"bamD3ToolTipTitle": "name",
+	"bamD3ToolTipAtributes":{
+	"Degree": "Number of Connections:"
+	},
+
+	"bamDataLoadAttributes":{
+		"fileName": 'data/iclaw.json',
+		"latAttribute": "repLat",
+		"lonAttribute": "repLon"
+	}
+};
